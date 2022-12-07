14b09aaf894e8a4808bc517ec663f81e5f04fd21
Iketyke <isaac.mellad.pemberton@gmail.com>
2022-12-07 09:59:53 +0000

    modify loading bar and change reviews to use useParams hook

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Home, Review, Search } from "./Pages";
 import { Header } from "./Components";
 
 const App = () => {
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 800);
-  }, [navigate]);
   return (
     <div className="App">
       <Header />
-      {loading ? (
-        <div className="App__preloader">
-          <h1>Loading...</h1>
-        </div>
-      ) : (
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Review/*" element={<Review />} />
-          <Route path="/Search" element={<Search />} />
-        </Routes>
-      )}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Review/:review_id" element={<Review />} />
+        <Route path="/Search" element={<Search />} />
+      </Routes>
     </div>
   );
 };
--- src/Pages/Review/Review.jsx
+++ src/Pages/Review/Review.jsx
@@ -1,35 +1,46 @@
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { getReview } from "../../utils";
 import "./Review.css";
 
 const Review = () => {
   const [currReview, setCurrReview] = useState({});
-  const location = useLocation();
-  console.log(location.pathname.split("/").reverse()[0])
+  const { review_id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    getReview(location.pathname.split("/").reverse()[0]).then((data) =>
-      setCurrReview(data.review)
-    );
-  }, [location]);
-  console.log(currReview)
+    getReview(review_id).then((data) => {
+      setCurrReview(data.review);
+      setIsLoading(false);
+    });
+  }, [review_id]);
+  console.log(currReview);
   return (
-    <div className="App__Review">
-      <div className="App__Review-image">
-        <img src={currReview.review_img_url} alt={currReview.title} />
-      </div>
-      <div className="App__Review-content">
-        <h2>{currReview.title}</h2>
-        <p>Designed by {currReview.designer}</p>
-        
-        <div className="App__Review-info">
-          <p>{currReview.created_at}</p>
-          <p>{currReview.category}</p>
+    <>
+      {" "}
+      {isLoading ? (
+        <div className="App__Review-loading">
+          <h1>Loading...</h1>
         </div>
-        <p>{currReview.review_body}</p>
-        <p className="App__Review-by">Review by {currReview.owner}</p>
-      </div>
-    </div>
+      ) : (
+        <div className="App__Review">
+          <div className="App__Review-image">
+            <img src={currReview.review_img_url} alt={currReview.title} />
+          </div>
+          <div className="App__Review-content">
+            <h2>{currReview.title}</h2>
+            <p>Designed by {currReview.designer}</p>
+
+            <div className="App__Review-info">
+              <p>{currReview.created_at}</p>
+              <p>{currReview.category}</p>
+            </div>
+            <p>{currReview.review_body}</p>
+            <p className="App__Review-by">Review by {currReview.owner}</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
